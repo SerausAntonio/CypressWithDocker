816a2e48934d949182bad46298d6b33b00bd301f
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-08-23 20:09:30 +0200

    forth commit

--- cypress/e2e/HooksConduit.cy.js
+++ cypress/e2e/HooksConduit.cy.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress"/>
 describe('Hooks Conduit',()=>{
     beforeEach('Log me in',()=>{
-
+        cy.visit("https://react-redux.realworld.io/");
+        cy.contains('Sign in').click();
+        cy.get('input[type="email"]').type('cypressdemo@gmail.com');
+        cy.get('input[type="password"]').type('cypressdemo');
+        cy.get('button[type="submit"]').click();
+        cy.contains('Settings').click();
+        cy.contains('Or click here to logout.').click();
 
     } )
     it('Create a new post',()=>{
-
+        cy.log(cy.url());
 
 
 
